3d6a69dd465e3751f021cc85f12d7118887ed59d
bindhu04092003 <134761386+bindhu04092003@users.noreply.github.com>
2026-05-31 19:12:20 +0530

    Update sales_dag.py

--- dags/sales_dag.py
+++ dags/sales_dag.py
@@ -1,1 +1,19 @@
 
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def sales_task():
+    print("Sales ETL Process Started")
+
+with DAG(
+    dag_id="sales_etl",
+    start_date=datetime(2025,1,1),
+    schedule="@daily",
+    catchup=False,
+) as dag:
+
+    task1 = PythonOperator(
+        task_id="sales_task",
+        python_callable=sales_task,
+    )
